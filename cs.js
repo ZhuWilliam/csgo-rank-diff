ea89773fd9df6926a84a1d579b05f9885088f0c5
WilliamZhu <william.zhu325@gmail.com>
2021-03-19 20:28:23 -0700

    Change submit button to on change

--- cs.js
+++ cs.js
@@ -31,38 +31,39 @@ class AverageRankForm extends React.Component {
         this.state = {team1Average: '0', team2Average: '0',
                       team1Total: '0', team2Total: '0',
                       ratio: '0', diff: '0'};
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
     
-    handleSubmit(event) {
-        const selects = document.querySelectorAll("select");
-        let team1 = 0, team2 = 0;
-        
-        let count = 0;
-        for (const sel of selects) {
-            if (count < 5) {
-                team1 += parseInt(sel.value);
-            }
-            else {
-                team2 += parseInt(sel.value);
-            }
-            count++;
-        }
-        
-        this.setState({team1Average: (team1 / 5)});
-        this.setState({team2Average: (team2 / 5)});
-        this.setState({team1Total: team1});
-        this.setState({team2Total: team2});
-        this.setState({ratio: (team1 / team2)});
-        this.setState({diff: Math.abs(team1 - team2)});
-        
-        event.preventDefault();
+    componentDidMount() {
+        document.querySelectorAll("select").forEach(sel => {
+            sel.addEventListener("change", event => {
+                const selects = document.querySelectorAll("select");
+                let team1 = 0, team2 = 0;
+                
+                let count = 0;
+                for (const sel of selects) {
+                    if (count < 5) {
+                        team1 += parseInt(sel.value);
+                    }
+                    else {
+                        team2 += parseInt(sel.value);
+                    }
+                    count++;
+                }
+                
+                this.setState({team1Average: (team1 / 5)});
+                this.setState({team2Average: (team2 / 5)});
+                this.setState({team1Total: team1});
+                this.setState({team2Total: team2});
+                this.setState({ratio: (team1 / team2)});
+                this.setState({diff: Math.abs(team1 - team2)});                
+            });
+        });
     }
 
     render() {
         return (
             <React.Fragment>
-                <form onSubmit={this.handleSubmit}>
+                <form>
                     <div>
                         team 1
                         <RankSelect />
@@ -79,16 +80,14 @@ class AverageRankForm extends React.Component {
                         <RankSelect />
                         <RankSelect />
                     </div>
-                    <br />
-                    <input type="submit" value="Submit" />
                 </form>
-                <br /><br />
+                <br />
                 <div>Totals: {this.state.team1Total} - {this.state.team2Total}</div>
-                <br /><br />
+                <br />
                 <div>Averages: {this.state.team1Average} - {this.state.team2Average}</div>
-                <br /><br />
+                <br />
                 <div>Ratio: {this.state.ratio}</div>
-                <br /><br />
+                <br />
                 <div>Diff: {this.state.diff}</div>
             </React.Fragment>
         );
